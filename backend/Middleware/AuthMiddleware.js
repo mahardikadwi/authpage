7b050a54a5d511993829545b615193a4c7473ce0
mahardikadwi <dwimahardika08@gmail.com>
2024-11-24 15:13:10 +0700

    added optional chaining

--- backend/Middleware/AuthMiddleware.js
+++ backend/Middleware/AuthMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const AuthMiddleware = (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
 
     if(!token) {
         return res.status(401).json({ message: "You must login" });
