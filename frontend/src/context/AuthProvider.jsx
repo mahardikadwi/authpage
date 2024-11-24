4ae6c4b335be5938f23339d1f5decffcbbfca642
mahardikadwi <dwimahardika08@gmail.com>
2024-11-24 15:41:49 +0700

    changing initial variable name~

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -3,13 +3,13 @@ import { createContext, useState } from "react";
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [authData, setAuthData] = useState(null);
+  const [auth, setAuthData] = useState(null);
   const setAuth = (userData) => {
     setAuthData(userData);
   };
 
   return (
-    <AuthContext.Provider value={{ authData, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
     </AuthContext.Provider>
   );
