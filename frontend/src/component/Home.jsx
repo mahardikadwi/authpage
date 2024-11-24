4852f111d8a8dbdc103a3afc920e33fcf18e9927
mahardikadwi <dwimahardika08@gmail.com>
2024-11-24 15:40:02 +0700

    removing unneccesary context hook

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,22 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
-  const { auth } = useContext(AuthContext);
-
   return (
     <div>
       <h1>Welcome to the Home Page!</h1>
       <p>Choose an option to continue:</p>
       <div>
-        {auth ? (
-          <p>Hello, {auth?.username}! Welcome to App!</p>
-        ) : (
-          <Link to="/auth">
-            <button>Login / Sign up</button>
-          </Link>
-        )}
+        <Link to="/auth">
+          <button>Login / Sign up</button>
+        </Link>
       </div>
     </div>
   );
